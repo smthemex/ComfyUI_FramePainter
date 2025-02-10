d51e910ddaf70c8407a2c1c8541a073c4d428681
smthemex <138738845+smthemex@users.noreply.github.com>
2025-02-10 09:37:31 +0800

    Update FramePainter_node.py

--- FramePainter_node.py
+++ FramePainter_node.py
@@ -6,7 +6,7 @@ import gc
 import numpy as np
 from diffusers import AutoencoderKLTemporalDecoder
 from diffusers.schedulers import EulerDiscreteScheduler
-
+from omegaconf import OmegaConf
 from PIL import Image
 from diffusers import AutoencoderKLTemporalDecoder, EulerDiscreteScheduler
 from safetensors.torch import load_file
@@ -15,7 +15,7 @@ from .modules.sparse_control_encoder import SparseControlEncoder
 from .modules.unet_spatio_temporal_condition_edit import UNetSpatioTemporalConditionEdit
 from .modules.attention_processors import MatchingAttnProcessor2_0
 from .modules.utils.attention_utils import set_matching_attention, set_matching_attention_processor
-from .node_utils import  process_image_with_mask,timer,pil2narry
+from .node_utils import  process_image_with_mask,timer,pil2narry,tensor_upscale,convert_cf2diffuser
 import folder_paths
 
 
@@ -44,7 +44,7 @@ class FramePainter_Loader:
                                  "encoder" in i]
         return {
             "required": {
-                "svd_repo": ("STRING", {"default": "stabilityai/stable-video-diffusion-img2vid-xt-1-1"}),
+                "model": ("MODEL",),
                 "FramePainter_unet": (["none"] + FramePainter_unet_list,),
                 "sparse_control_ckpt": (["none"] + sparse_control_ckpt_list,),
             },
@@ -55,7 +55,7 @@ class FramePainter_Loader:
     FUNCTION = "loader_main"
     CATEGORY = "FramePainter"
 
-    def loader_main(self,svd_repo,FramePainter_unet,sparse_control_ckpt):
+    def loader_main(self,model,FramePainter_unet,sparse_control_ckpt):
 
         if FramePainter_unet!="none":  
             FramePainter_unet=folder_paths.get_full_path("FramePainter", FramePainter_unet)
@@ -66,28 +66,35 @@ class FramePainter_Loader:
         else:
             raise ValueError("Please select a valid sparse_control_ckpt.")
         
+        svd_repo = os.path.join(current_node_path, "svd_repo")
         # load model
         print("***********Load model ***********")
+        unet_config_file=os.path.join(svd_repo, "unet")
+        unet=convert_cf2diffuser(model.model,unet_config_file)
 
-        unet = UNetSpatioTemporalConditionEdit.from_pretrained(
-            svd_repo,
-            subfolder="unet",
-            low_cpu_mem_usage=True,
-            variant="fp16"
-        )
+        # unet = UNetSpatioTemporalConditionEdit.from_pretrained(
+        #     svd_repo,
+        #     subfolder="unet",
+        #     low_cpu_mem_usage=True,
+        #     variant="fp16"
+        # )
+
         sparse_control_encoder = SparseControlEncoder()
 
-        vae = AutoencoderKLTemporalDecoder.from_pretrained(
-            svd_repo, subfolder="vae",variant="fp16")
+        # vae = AutoencoderKLTemporalDecoder.from_pretrained(
+        #     svd_repo, subfolder="vae",variant="fp16")
+        vae_config=os.path.join(svd_repo, "vae/config.json")
+        vae_config=OmegaConf.load(vae_config)
         noise_scheduler = EulerDiscreteScheduler.from_pretrained(
             svd_repo, subfolder="scheduler")
         pipeline = FramePainterPipeline.from_pretrained(
             svd_repo,
             sparse_control_encoder=sparse_control_encoder, 
             unet=unet,
-            vae=vae,
+            vae_config=vae_config,
             revision=None,
-            noise_scheduler=noise_scheduler
+            noise_scheduler=noise_scheduler,
+            #vae=vae,
             )
 
         set_matching_attention(pipeline.unet)
@@ -118,6 +125,7 @@ class FramePainter_Sampler:
             "required": {
                 "model": ("MODEL_FramePainter",),
                 "clip_vision": ("CLIP_VISION",),
+                "vae": ("VAE",),
                 "image": ("IMAGE",), # B H W C C=3
                 "mask": ("MASK",), # B H W 
                 "seed": ("INT", {"default": 0, "min": 0, "max": MAX_SEED}),
@@ -126,7 +134,8 @@ class FramePainter_Sampler:
                 "width": ("INT", {"default": 512, "min": 256, "max": 1920, "step": 64, "display": "number"}),
                 "height": ("INT", {"default": 512, "min": 256, "max": 1920, "step": 64, "display": "number"}),
                 "control_scale": ("FLOAT", {"default": 0.8, "min": 0.0, "max": 1.0,"step": 0.05}),
-                }}
+                },
+                }
                          
         
     RETURN_TYPES = ("IMAGE",)
@@ -135,10 +144,10 @@ class FramePainter_Sampler:
     CATEGORY = "FramePainter"
     
     
-    def sampler_main(self, model,clip_vision,image,mask,seed,steps,guidance_scale,width,height,control_scale):
+    def sampler_main(self, model,clip_vision,vae,image,mask,seed,steps,guidance_scale,width,height,control_scale):
 
         model.to("cuda")
-       
+        #vae=kwargs.get("vae", None)
         image_embeds = clip_vision.encode_image(image)["image_embeds"] #torch.Size([1, 1024])
         image_embeds=image_embeds.clone().detach().to(device, dtype=torch.float16) # dtype需要改成可选
 
@@ -151,6 +160,16 @@ class FramePainter_Sampler:
                 Image.new("RGB", (width, height), color=(0, 0, 0)), 
                 merged_image
             ]
+            if vae is not None:
+                validation_control_ = [np.array(pil2narry(image)) for image in validation_control_images]
+                validation_control_ = [torch.from_numpy(img).to(device, dtype=torch.float16) for img in validation_control_]
+                validation_control_ = torch.cat(validation_control_, dim=0)
+                input_latents=vae.encode(tensor_upscale(image,width,height)).to(device, dtype=torch.float16)
+                validation_control_images=validation_control_.unsqueeze(0).permute(0,1,4,2,3).to(device, dtype=torch.float16)
+                negative_image_latents = torch.zeros_like(input_latents)
+                input_latents=torch.cat([negative_image_latents, input_latents])
+            else:
+                raise "vae is None"
             result = model(
                 input_image_resized, 
                 validation_control_images,
@@ -160,13 +179,20 @@ class FramePainter_Sampler:
                 guidance_scale=guidance_scale,
                 num_inference_steps=steps,
                 generator=torch.Generator().manual_seed(seed),
+                output_type="latent",#out lantents
                 image_embs=image_embeds,
+                input_latents=input_latents,
+               
             ).frames[0],
-            
-        image = result[0][1]
+        
+        b,_,_,_=result[0].shape
+        last= torch.chunk(result[0], chunks=b)
+        image=vae.decode(last[-1])
         gc.collect()
         torch.cuda.empty_cache()
-        return (pil2narry(image),)
+        return (image,)
+        #image = result[0][1]
+        #return (pil2narry(image),out,)
 
 
 
